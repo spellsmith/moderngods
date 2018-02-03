2268cddc8b5fba563844f9baf53f95e0f08e381c
Spellsmith <spellsmithtsw@gmail.com>
2018-02-02 23:22:03 -0600

    Modifed leave/arrive messages for traverse

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -31,4 +31,107 @@ class Character(GenderCharacter, ContribRPCharacter):
     at_post_puppet - Echoes "AccountName has entered the game" to the room.
 
     """
-    pass
+    def at_after_move(self, source_location, **kwargs):
+        """
+        We make sure to look around after a move.
+        """
+        if self.location.access(self, "view"):
+            self.msg(self.at_look(self.location))
+        prompt = "Enter Command:"
+        self.msg("\n", prompt=prompt)
+
+
+    def announce_move_from(self, destination, msg=None, mapping=None, **kwargs):
+        """
+        Called if the move is to be announced. This is
+        called while we are still standing in the old
+        location.
+        Args:
+            destination (Object): The place we are going to.
+            msg (str, optional): a replacement message.
+            mapping (dict, optional): additional mapping objects.
+            **kwargs (dict): Arbitrary, optional arguments for users
+                overriding the call (unused by default).
+        You can override this method and call its parent with a
+        message to simply change the default message.  In the string,
+        you can use the following as mappings (between braces):
+            object: the object which is moving.
+            exit: the exit from which the object is moving (if found).
+            origin: the location of the object before the move.
+            destination: the location of the object after moving.
+        """
+        if not self.location:
+            return
+        if msg:
+            string = msg
+        else:
+            string = "{object} leaves towards {exit}."
+
+        location = self.location
+        exits = [o for o in location.contents if o.location is location and o.destination is destination]
+        if not mapping:
+            mapping = {}
+
+        mapping.update({
+            "object": self,
+            "exit": "the " + str(exits[0]) if exits else "somewhere",
+            "origin": location or "nowhere",
+            "destination": destination or "nowhere",
+        })
+
+        location.msg_contents(string, exclude=(self, ), mapping=mapping)
+
+    def announce_move_to(self, source_location, msg=None, mapping=None, **kwargs):
+        """
+        Called after the move if the move was not quiet. At this point
+        we are standing in the new location.
+        Args:
+            source_location (Object): The place we came from
+            msg (str, optional): the replacement message if location.
+            mapping (dict, optional): additional mapping objects.
+            **kwargs (dict): Arbitrary, optional arguments for users
+                overriding the call (unused by default).
+        Notes:
+            You can override this method and call its parent with a
+            message to simply change the default message.  In the string,
+            you can use the following as mappings (between braces):
+                object: the object which is moving.
+                exit: the exit from which the object is moving (if found).
+                origin: the location of the object before the move.
+                destination: the location of the object after moving.
+        """
+
+        if not source_location and self.location.has_account:
+            # This was created from nowhere and added to an account's
+            # inventory; it's probably the result of a create command.
+            string = "You now have %s in your possession." % self.get_display_name(self.location)
+            self.location.msg(string)
+            return
+
+        if source_location:
+            if msg:
+                string = msg
+            else:
+                string = "{object} arrives from {exit}."
+        else:
+            string = "{object} arrives to {destination}."
+
+        origin = source_location
+        destination = self.location
+        exits = []
+        if origin:
+            exits = [o for o in destination.contents if o.location is destination and o.destination is origin]
+
+        if not mapping:
+            mapping = {}
+
+        mapping.update({
+            "object": self,
+            "exit": "the " + str(exits[0]) if exits else "somewhere",
+            "origin": origin or "nowhere",
+            "destination": destination or "nowhere",
+        })
+
+        destination.msg_contents(string, exclude=(self, ), mapping=mapping)
+
+        pass
