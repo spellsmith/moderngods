7aa081325c2a77bd06b788835f0b29d9ac352a94
Spellsmith <spellsmithtsw@gmail.com>
2018-02-02 23:20:39 -0600

    Changed display of exits, chars, objs

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -19,4 +19,40 @@ class Room(ExtendedRoom, ContribRPRoom):
     See examples/object.py for a list of
     properties and methods available on all Objects.
     """
+    def return_appearance(self, looker, **kwargs):
+        """
+        This formats a description. It is the hook a 'look' command
+        should call.
+        Args:
+            looker (Object): Object doing the looking.
+            **kwargs (dict): Arbitrary, optional arguments for users
+                overriding the call (unused by default).
+        """
+        if not looker:
+            return ""
+        # get and identify all objects
+        visible = (con for con in self.contents if con != looker and
+                   con.access(looker, "view"))
+        exits, users, things = [], [], []
+        for con in visible:
+            key = con.get_display_name(looker)
+            if con.destination:
+                exits.append(key)
+            elif con.has_account:
+                users.append("|c%s|n" % key)
+            else:
+                things.append(key)
+        # get description, build string
+        string = "|c%s|n\n" % self.get_display_name(looker)
+        desc = self.db.desc
+        if desc:
+            string += "%s" % desc
+        if exits:
+            string += "\n\n|yExits:|n " + ", ".join(exits)
+        if users:
+            string += "\n\n|gPeople:|n " + ", ".join(users)
+        if things:
+            string += "\n\n|mYou see:|n " + ",".join(things)
+        return string
+
     pass
